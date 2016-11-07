bc2fc82d50c96f0e3766671d9059092f39bf949f
Nan Min <nanutza@gmail.com>
2016-11-07 17:37:51 -0500

    Create List controllers / views

--- app/controllers/list.rb
+++ app/controllers/list.rb
@@ -1,1 +1,57 @@
-# before_filter :require_login
+get '/lists' do
+  @lists = List.all
+  erb :'lists/index'
+end
+
+before '/lists*' do
+  require_login
+end
+
+get '/lists/new' do
+
+  if request.xhr?
+    erb :'_new_list', layout: false
+  else
+  erb :'lists/new'
+  end
+end
+
+post '/lists' do
+    list = List.new(params[:list])
+    list[:user_id]=current_user.id
+    if list.save
+      redirect '/'
+    else
+      @errors=list.errors.full_messages
+      erb :'lists/new'
+    end
+end
+
+get '/lists/:id' do
+  @list = List.find(params[:id])
+  erb :'lists/show'
+end
+
+get '/lists/:id/edit' do
+  require_login
+  @list = List.find_by(id: params[:id])
+  erb :'lists/edit'
+end
+
+put '/lists/:id' do
+    # binding.pry
+    @list = List.find_by(id: params[:id])
+    @list[:user_id]=current_user.id
+    if @list.update(params[:list])
+      redirect '/users/:id'
+    else
+      @errors=lists.errors.full_messages
+      erb :'lists/edit'
+    end
+end
+
+delete '/lists/:id' do
+  @list = List.find_by(id: params[:id])
+  @list.destroy
+  redirect '/users/:id'
+end
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,5 +1,5 @@
 get '/users' do
-  erb :'users.index'
+  erb :'users/index'
 end
 
 get '/users/new' do
@@ -10,9 +10,18 @@ post '/users' do
   user = User.new(params[:user])
   user.avatar = Faker::Avatar.image(Faker::Lorem.word, "80x80")
 
-  if user.save
+  if user.save && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
     redirect '/users'
   else
+    @errors = user.errors.full_messages
     erb :'users/new'
   end
+end
+
+get '/users/:id' do
+  require_login
+  @user = current_user
+  @lists = @user.lists
+  erb :'users/show'
 end
